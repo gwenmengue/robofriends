8556805038b820525aa211ef409a356d0e1b485d
gwenmengue <gwenmengue@users.noreply.github.com>
2019-11-05 21:51:01 +0100

    Updates

---
+++ precache-manifest.b41ee5bce2b8787900664352d70720f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f69688dbc7d8630f7ad54cee4a78a187",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "10ad3766526660d9fe0a",
+    "url": "/robofriends/static/css/main.1ae6f995.chunk.css"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriends/static/js/2.2a21f297.chunk.js"
+  },
+  {
+    "revision": "10ad3766526660d9fe0a",
+    "url": "/robofriends/static/js/main.02f8969c.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "a64ed299ab6012fdd5d8b0d5c3e9236e",
+    "url": "/robofriends/static/media/Viafont.a64ed299.woff"
+  }
+]);
